38843d6ba45084e23c3b548f50a6f1215a2e9450
BanceDev <lanceaborden@protonmail.com>
2024-08-06 20:44:07 -0400

    main page completed

--- app.js
+++ app.js
@@ -5,18 +5,55 @@ document.addEventListener("alpine:init", () => {
     projects: [
       {
         id: 1,
-        img: "./assets/spense.png",
+        img: "./assets/quakeprism.png",
         imgAlt: "quake prism",
         title: "Quake Prism",
         body: "Quake Prism is an open source game engine editor for the Quake engine made by iD Software. This tool is an all in one suite for developers to make mods and games.",
       },
       {
         id: 2,
-        img: "./assets/yelp-camp.png",
+        img: "./assets/xed.png",
         imgAlt: "linux mint xed",
         title: "Linux Mint Xed",
-        body: "Xed is an open source text editor written in C and part of the Linux Mint xapps software suite. I have contributed to the development of this software mostly through the fixing of bugs and memory/performance issues as a way to improve at C.",
+        body: "Xed is an open source text editor written in C and part of the Linux Mint xapps software suite. Contributed to the development of this software mostly through the fixing of bugs and memory/performance issues as a way to improve at C.",
       },
+      {
+        id: 3,
+        img: "./assets/lunabotics.jpg",
+        imgAlt: "lunabotics",
+        title: "Lunabotics",
+        body: "Developed the driver station and networked controls for the rookie year OSU Lunabotics robot. We received the Nova Award for Stellar Systems Engineering Performance by a First Year Team.",
+      },
+      {
+        id: 4,
+        img: "./assets/landelver.png",
+        imgAlt: "landelver",
+        title: "LANdelver",
+        body: "LANdelver is a virtual tabletop for the in person Dungeons & Dragons expereince. Designed to enhance the possibilities of using a screen table with a fully custom Lua to C API for scripting enemy AI along with an intuitive web interface for players to connect from."
+      },
+    ],
+    work: [
+      {
+        id: 1,
+        img: "./assets/enable.jpg",
+        imgAlt: "enable injections",
+        title: "Enable Injections",
+        body: "Aided in product development of vial transfer system through design, implementation, and data analysis of product validation tests. Developed manufacturing and mechanical design skills.",
+      },
+      {
+        id: 2,
+        img: "./assets/osueed.jpg",
+        imgAlt: "osu engineering education department",
+        title: "OSU Engineering Education Department",
+        body: "Supported first year students in machining robots for engineering courses. Assisted departure from an old proprietary microcontroller to a new arduino based system by porting firmware libraries.",
+      },
+      {
+        id: 3,
+        img: "./assets/letsstartcoding.png",
+        imgAlt: "lets start coding",
+        title: "Let's Start Coding",
+        body: "Developed Arduino code used as projects for new and repurposed STEM kits. Created instructional content to teach various coding concepts related to the projects. Helped develop a new chapter of challenging content to expand current content base.",
+      }
     ],
     logos: [
       {
