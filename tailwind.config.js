99ee4c847d5df4f8caa19bc7156548ca9f81dd4a
BanceDev <lanceaborden@protonmail.com>
2025-09-30 17:41:07 -0400

    removed blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}", "./blog/*.{html,js}"],
+  content: ["*.{html,js}"],
   theme: {
     extend: {},
     fontFamily: {
